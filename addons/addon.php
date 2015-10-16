62d876370862a54e25b781ca91cd96208e718526
Devon Endicott <dephurites@gmail.com>
2015-10-16 10:55:58 -0400

    styling changes

--- addons/addon.php
+++ addons/addon.php
@@ -25,9 +25,9 @@ require_once(realpath(dirname(__DIR__) . "/private/class/BoardManager.php"));
     My add-on does a lot of cool stuff. There's so many reasons why you should get it. First off, it has an incredible small footprint because it <b>literally does nothing</b>! Thats right: the mod is so efficient that it has no impact on your game whatsoever.<br /><br />
     Lets get to 1337 downloads guys!
   </p>
-  <a href="dl.php" class="btn green"><b>Stable</b><span style="font-size:9pt;line-height: 10px;text-align:center;"><br />v1.1.0</span></a><br />
-  <a href="dl.php" class="btn yellow"><b>Unstable</b><span style="font-size:9pt;line-height: 10px;text-align:center;"><br />v1.1.0-alpha.1</span></a><br />
-  <a href="dl.php" class="btn red"><b>Development</b><span style="font-size:9pt;line-height: 10px;text-align:center;"><br />v1.1.0-alpha.6</span></a><br />
+  <a href="dl.php" class="btn dlbtn green"><b>Stable</b><span style="font-size:9pt"><br />v1.1.0</span></a>
+  <a href="dl.php" class="btn dlbtn yellow"><b>Unstable</b><span style="font-size:9pt"><br />v1.1.0-alpha.1</span></a>
+  <a href="dl.php" class="btn dlbtn red"><b>Development</b><span style="font-size:9pt"><br />v1.1.0-alpha.6</span></a><br />
 </div>
 
 <?php require_once(realpath(dirname(__DIR__) . "/private/footer.php")); ?>
