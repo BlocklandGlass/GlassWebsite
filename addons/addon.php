ea4c4713c8791999ca445b522bf91ff1781de46d
Devon Endicott <dephurites@gmail.com>
2015-10-16 10:15:39 -0400

    foundation for add-on page

---
+++ addons/addon.php
@@ -0,0 +1,33 @@
+<?php
+$_PAGETITLE = "Glass | Add-On Name";
+require_once(realpath(dirname(__DIR__) . "/private/header.php"));
+
+require_once(realpath(dirname(__DIR__) . "/private/class/BoardManager.php"));
+
+//LEGACY CODE, EXAMPLE ONLY
+//require_once('../../html/class/BoardManager.php');
+?>
+
+<div class="navcontainer">
+  <div class="navcontent">
+    <!-- temporary nav -->
+	  <a class="homebtn" href="/">Blockland Glass</a>
+  </div>
+</div>
+<div class="maincontainer">
+  <span style="font-size: 9pt;"><a href="/addons/">Add-Ons</a> >> <a href="#">Some Board</a> >> <a href="#">My Add-On</a></span>
+	<h2>My Add-On</h2>
+  <p>
+    <image src="http://blocklandglass.com/icon/icons32/user.png" /> By <a href="#">Jincux</a><br />
+    <image src="http://blocklandglass.com/icon/icons32/cog.png" /> Some Board
+  </p>
+  <p>
+    My add-on does a lot of cool stuff. There's so many reasons why you should get it. First off, it has an incredible small footprint because it <b>literally does nothing</b>! Thats right: the mod is so efficient that it has no impact on your game whatsoever.<br /><br />
+    Lets get to 1337 downloads guys!
+  </p>
+  <a href="dl.php" class="btn green"><b>Stable</b><span style="font-size:9pt;line-height: 10px;text-align:center;"><br />v1.1.0</span></a><br />
+  <a href="dl.php" class="btn yellow"><b>Unstable</b><span style="font-size:9pt;line-height: 10px;text-align:center;"><br />v1.1.0-alpha.1</span></a><br />
+  <a href="dl.php" class="btn red"><b>Development</b><span style="font-size:9pt;line-height: 10px;text-align:center;"><br />v1.1.0-alpha.6</span></a><br />
+</div>
+
+<?php require_once(realpath(dirname(__DIR__) . "/private/footer.php")); ?>
