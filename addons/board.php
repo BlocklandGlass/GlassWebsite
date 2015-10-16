2e63f961f2d371e162f611371d4a98506a2a2456
Jincux <dephurites@gmail.com>
2015-10-16 15:29:07 -0400

    raw import of addonobject/manager, boards display add-ons

--- addons/board.php
+++ addons/board.php
@@ -1,12 +1,14 @@
 <?php
 require_once(realpath(dirname(__DIR__) . "/private/class/BoardManager.php"));
+require_once(realpath(dirname(__DIR__) . "/private/class/AddonManager.php"));
+require_once(realpath(dirname(__DIR__) . "/private/class/AddonObject.php"));
 
 if(isset($_GET['id'])) {
   try {
     $boardObject = BoardManager::getFromId($_GET['id']);
   } catch(Exception $e) {
     //board doesn't exist
-    //header('Location: /addons');
+    header('Location: /addons');
     die("board doesnt exist");
   }
 } else {
@@ -14,11 +16,12 @@ if(isset($_GET['id'])) {
   die();
 }
 
-$_PAGETITLE = "Glass | Board Name";
+$_PAGETITLE = "Glass | " . $boardObject->getName();
 require_once(realpath(dirname(__DIR__) . "/private/header.php"));
 require_once(realpath(dirname(__DIR__) . "/private/navigationbar.php"));
 ?>
 <div class="maincontainer">
+  <h1 style="text-align:center"><?php echo $boardObject->getName(); ?></h1>
   <a href="/addons">Add-Ons</a> >> <a href="#"><?php echo $boardObject->getName() ?></a>
   <div class="pagenav">
     <a href="#">1</a>
@@ -34,33 +37,29 @@ require_once(realpath(dirname(__DIR__) . "/private/navigationbar.php"));
 	<table class="boardtable">
     <tbody>
       <tr class="boardheader">
-        <td colspan="4"><?php echo $boardObject->getName(); ?></td>
-      </tr>
-      <tr class="boardheader">
         <td>Name</td>
         <td>Author(s)</td>
         <td>Rating</td>
         <td>Downloads</td>
       </tr>
-      <tr>
-        <td style="width: 50%">My Add-On</td>
-        <td><a href="#">Jincux</a> and <a href="#">Nexus</a></td>
-        <td>
-          <image src="http://blocklandglass.com/icon/icons16/star.png" />
-          <image src="http://blocklandglass.com/icon/icons16/star.png" />
-          <image src="http://blocklandglass.com/icon/icons16/star.png" />
-          <image src="http://blocklandglass.com/icon/icons16/star.png" />
-          <image src="http://blocklandglass.com/icon/icons16/star.png" />
-        </td>
-        <td>413</td>
-      </tr>
       <?php
-      /*
       $addons = $boardObject->getAddons();
 			foreach($addons as $addon) {
-
+        ?>
+        <tr>
+          <td style="width: 50%"><?php echo $addon->getName() ?></td>
+          <td><a href="#">Jincux</a> and <a href="#">Nexus</a></td>
+          <td>
+            <image src="http://blocklandglass.com/icon/icons16/star.png" />
+            <image src="http://blocklandglass.com/icon/icons16/star.png" />
+            <image src="http://blocklandglass.com/icon/icons16/star.png" />
+            <image src="http://blocklandglass.com/icon/icons16/star.png" />
+            <image src="http://blocklandglass.com/icon/icons16/star.png" />
+          </td>
+          <td>413</td>
+        </tr>
+        <?php
       }
-      */
       ?>
       <tr class="boardheader">
         <td colspan="4"></td>
