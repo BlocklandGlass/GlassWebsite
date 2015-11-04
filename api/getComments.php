cf06e3530c6b5b783988046ef546318346011dbe
hoff121324 <hoff121324@gmail.com>
2015-11-03 22:44:48 -0600

    wait did these files not get added?

---
+++ api/getComments.php
@@ -0,0 +1,41 @@
+<table>
+<tbody>
+<?php
+	//This page is designed to be requested by ajax
+	//I also want it to be possible to request this content in-game.
+	//In the future the file that actually interacts with the database should be in /private/class, while this one processes get requests and formats data
+
+	require_once(realpath(dirname(__DIR__) . "/private/class/DatabaseManager.php"));
+	$database = new DatabaseManager();
+	$result = $database->query("SELECT * FROM `addon_comments` WHERE `aid` = '" . $database->sanitize($_GET['id']) . "'");
+
+	if(!$result) {
+		echo("Database error: " . $database->error());
+	} else {
+		if($result->num_rows == 0) {
+			echo("<tr style=\"vertical-align:top\">");
+			echo("<td style=\"width: 150px;\">");
+			echo("There are no comments here yet.");
+			echo("</td></tr>");
+		} else {
+			require_once(realpath(dirname(__DIR__) . "/private/class/UserHandler.php"));
+
+			while($row = $result->fetch_object()) {
+				$user = UserManager::getFromId($row->uid);
+				echo("<tr style=\"vertical-align:top\">");
+				echo("<td style=\"width: 150px;\">");
+				echo("<a href=\"/user/view.php?id=" . $user->getID() . "\">" . htmlspecialchars($user->getUsername()) . "</a>");
+
+				//Not sure where administrator status is stored.  My guess is 'groups' but I can't be certain.
+				//At any rate, we should probably go and rethink the database tables for long term use.
+				echo("<br /><span style=\"font-size: .8em;\">" . $user->getBLID() . "<br />Administrator?</span>");
+				echo("</td><td>");
+				echo(htmlspecialchars($row->comment));
+				echo("</td></tr>");
+			}
+		}
+		$result->close();
+	}
+?>
+</tbody>
+</table>
---
+++ api/search.php
@@ -0,0 +1,60 @@
+<?php
+	//this page is designed to be requested by ajax or the in-game client
+
+	require_once(realpath(dirname(__DIR__) . "/private/class/DatabaseManager.php"));
+	require_once(realpath(dirname(__DIR__) . "/private/lib/Parsedown.php"));
+
+	if(!isset($_POST['query'])) {
+		echo("Invalid search");
+	} else {
+		$db = new DatabaseManager();
+		$baseQuery = "SELECT * FROM `addon_addons` WHERE `name` LIKE '%" . $db->sanitize($_POST['query']) . "%'";
+
+		//later on we can make it so administrators can search for deleted add-ons
+		$extendedQuery = " AND `deleted` = 0";
+
+		if(isset($_POST['blid'])) {
+			try {
+				require_once(realpath(dirname(__DIR__) . "/private/class/UserManager.php"));
+				$user = UserManager::getFromBLID($_POST['blid']);
+				$extendedQuery = $extendedQuery . " AND `author` = '" . $db->sanitize($_POST['blid']) . "'";
+			} catch(Exception $e) {
+				echo("<p>User " . htmlspecialchars($_POST['blid']) . " not found.</p>");
+			}
+		}
+
+		//One of the few time's we'll use a direct SQL query on a page
+		$result = $db->query($baseQuery . $extendedQuery);
+
+		echo("<h2>Search Results for ");
+		echo("\"<u>" . htmlspecialchars($_POST['query']) . "</u>\"");
+
+		if(isset($user) && $user) {
+			echo(" by <a href=\"/user/view.php?id=" . $user->getID() . "\">" . htmlspecialchars($user->getUsername()) . "</a>");
+		}
+		echo("</h2><hr />");
+
+		if($result->num_rows) {
+			while($row = $result->fetch_object()) {
+				echo "<p><b><a href=\"addon.php?id=" . $row->id . "\">" . htmlspecialchars($row->name) . "</a></b><br />";
+
+				if(strlen($row->description) > 200) {
+					$desc = substr($row->description, 0, 196) . " ...";
+				} else {
+					$desc = $row->description;
+				}
+
+				$Parsedown = new Parsedown();
+				$Parsedown->setBreaksEnabled(true);
+				$Parsedown->setMarkupEscaped(true);
+
+				//may need escaping
+				echo $Parsedown->text($desc);
+				echo "</p><br />";
+			}
+		} else {
+			echo "We couldn't find anything. Sorry about that.";
+		}
+		$result->close();
+	}
+?>
