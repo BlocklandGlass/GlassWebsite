c9d0aada1b8691c20dc6b3a6e08287f820f2b2a1
hoff121324 <hoff121324@gmail.com>
2015-11-05 16:23:15 -0600

    added some basic unit tests

---
+++ test/databaseTest.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../private/class/databaseManager.php");
+
+class DatabaseTest extends PHPUnit_Framework_TestCase {
+	public function testError() {
+		$database = new DatabaseManager();
+		$resource = $database->query("SELECT `garbage` FROM `thingthatdoesnotexist`");
+		$this->assertEquals(false, $resource);
+		$this->assertNotEquals("", $database->error());
+	}
+
+	public function testBasicQuery() {
+		$database = new DatabaseManager();
+		$resource = $database->query("SHOW TABLES");
+		$this->assertNotEquals(false, $resource);
+	}
+}
+?>
---
+++ test/userTest.php
@@ -0,0 +1,11 @@
+<?php
+require_once("../private/class/databaseManager.php");
+
+class UserTest extends PHPUnit_Framework_TestCase {
+	public function testRegister() {
+		//user registration system will change from current format to an email based one
+		//code from register.php, login.php, and logout.php will all be merged into a static class
+		//no point writing tests for old system now
+	}
+}
+?>
