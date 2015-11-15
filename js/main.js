5164a8230b6bad1a4dd1c1b96e310d42d94ca867
hoff121324 <hoff121324@gmail.com>
2015-11-15 05:53:45 -0600

    added mobile menu

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 //idk just testing stuff
+function toggleMobileMenu() {
+	console.log("tog");
+	$(".mobilemenu").slideToggle();
+}
+
 function checkNavbar() {
+	if(document.getElementById("mobilemenubutton")) {
+		console.log("undo thing");
+		$("#navcontent > ul").removeClass("mobilemenu");
+		$("#mobilemenubutton").remove();
+	}
+
 	if($("#navcontent").height() > $("#navcontainer").height()) {
-		console.log("do thing!");
-		$("#navcontent > ul").addClass("mobilemenu").before("<div id='mobilemenubutton'>|||</div>");;
+		if(!document.getElementById("mobilemenubutton")) {
+			console.log("do thing!");
+			$("#navcontent > ul").addClass("mobilemenu").before("<a href='javascript:toggleMobileMenu();' id='mobilemenubutton'>|||</a>");
+		}
+	} else {
+		$("#navcontent > ul").show();
 	}
 }
 
 $(document).ready(function () {
 	checkNavbar()
-	$("#mobilemenubutton").click(function () {
-		$(".mobilemenu").toggle();
-	});
+	//$("#mobilemenubutton").click(function () {
+	//	$(".mobilemenu").slideToggle();
+	//});
 });
 $(window).resize(checkNavbar);
 
