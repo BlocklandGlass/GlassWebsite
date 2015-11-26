fe6807b4c2ccbe0eb210ccd28f76c2f6435d6707
Devon Endicott <dephurites@gmail.com>
2015-11-26 18:49:17 -0500

    a little work on addon upload, not familiar with the ajax

--- addons/upload/index.php
+++ addons/upload/index.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	require_once(realpath(dirname(__DIR__) . "/private/class/UserManager.php"));
+	require_once(realpath(dirname(__DIR__) . "/../private/class/UserManager.php"));
 	$user = UserManager::getCurrent();
 
 	if(!$user) {
@@ -15,18 +15,56 @@ include(realpath(dirname(dirname(__DIR__)) . "/private/navigationbar.php"));
 ?>
 <div class="maincontainer">
   <form action="upload.php" method="post">
-    Add-On Type: <select>
-      <option>Add-On</option>
-      <option>Print</option>
-      <option>Brick</option>
-    </select>
+		<table class="formtable">
+			<tbody>
+				<tr>
+					<td colspan="2"><h2>Upload (step 1 of 2)</h2></td>
+				</tr>
+				<tr>
+					<td><b>Name</b></td>
+					<td><input type="text" name="name" /></td>
+				</tr>
+				<tr>
+					<td>
+				    <b>Add-On Type</b><br />
+					</td>
+					<td>
+						<select>
+				      <option>Add-On</option>
+				      <option>Print</option>
+				      <option>Sound</option>
+				    </select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<span style="font-size: 0.8em">
+							<b>Add-On</b> - Anything with a .cs file!<br />
+							<b>Print</b> - Prints, faces, any of the likes<br />
+							<b>Sound</b> - Audio!
+						</span>
+					</td>
+				</tr>
+				<tr>
+					<td><b>Description</b></td>
+					<td><textarea style="font-size:0.8em;" rows="5" name="name" /></textarea></td>
+				</tr>
+				<tr>
+					<td>
+						<p><b>File</b></p>
+						<!--<span style="font-size: 0.7em;">You can find your saves in your Blockland folder!</span>-->
+						<!--<p class="description">You can find your saves in your Blockland folder!</p>-->
+					</td>
+					<td style="vertical-align: middle">
+						<input type="file" name="uploadfile" id="uploadfile">
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="submit" value="Upload File" name="submit"></td>
+				</tr>
+			</tbody>
+		</table>
   </form>
-  <p><b>Add-On</b><br />
-  Any ordinary add-on, including either a client.cs or server.cs file.</p>
-  <p><b>Add-On</b><br />
-  Any ordinary add-on, including either a client.cs or server.cs file.</p>
-  <p><b>Add-On</b><br />
-  Any ordinary add-on, including either a client.cs or server.cs file.</p>
 </div>
 
 <?php include(realpath(dirname(dirname(__DIR__)) . "/private/footer.php")); ?>
