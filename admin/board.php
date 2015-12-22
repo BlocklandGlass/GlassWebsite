ab94ceb71f69b02a467ba96425d5245b9d122086
Jincux <dephurites@gmail.com>
2015-12-22 14:17:16 -0500

    admin page to add boards

--- admin/board.php
+++ admin/board.php
@@ -3,27 +3,20 @@ require_once(realpath(dirname(__DIR__) . "/private/header.php"));
 require_once(realpath(dirname(__DIR__) . "/private/navigationbar.php"));
 require_once(realpath(dirname(__DIR__) . "/private/class/DatabaseManager.php"));
 
-if(isset($_POST['name']) && isset($_POST['icon'] && isset($_POST['cate'])) {
+if(isset($_POST['name']) && isset($_POST['icon']) && isset($_POST['desc'])) {
   $db = new DatabaseManager();
-  $db->query("INSERT INTO `addon_boards` VALUES (`id`, `name`, `icon`, `subCategory`) VALUES (NULL, '" . $db->sanitize($_POST['name']) . "', '" . $db->sanitize($_POST['icon']) . "', '" . $db->sanitize($_POST['cate']) . "');")
+  $db->query("INSERT INTO `addon_boards` (`id`, `name`, `video`, `description`) VALUES (NULL, '" . $db->sanitize($_POST['name']) . "', '" . $db->sanitize($_POST['icon']) . "', '" . $db->sanitize($_POST['desc']) . "');");
 }
 
 ?>
-
-<div class="bigheadcontainer">
-	<h1>Blockland Glass</h1>
-	<h2>A service for the community, by the community</h2>
-	<a href="dl.php" class="btn blue"><b>Download</b> v1.1.0-alpha.1</a><br />
-	<a href="http://blocklandglass.com" class="btn green">Classic Site</a><br />
-	<a href="addons" class="btn yellow">Add-Ons</a><br /><br />
-</div>
 <div class="maincontainer">
+  <form action="board.php" method="post">
   <table class="formtable">
     <tbody>
       <tr><td class="center" colspan="2"><h2>Log In</h2></td></tr>
       <tr><td>Board Name:</td><td><input type="text" name="name" id="name"></td></tr>
       <tr><td>Icon:</td><td><input type="text" name="icon" id="icon"></td></tr>
-      <tr><td>Sub Category:</td><td><input type="text" name="cate" id="cate"></td></tr>
+      <tr><td>Description:</td><td><textarea name="desc" id="desc"></textarea></tr>
       <tr><td class="center" colspan="2"><input type="submit"></td></tr>
     </tbody>
   </table>
@@ -33,6 +26,7 @@ if(isset($_POST['name']) && isset($_POST['icon'] && isset($_POST['cate'])) {
       echo("<input type=\"hidden\" name=\"redirect\" value=\"" . htmlspecialchars($_POST['redirect']) . "\">");
     }
   ?>
+  </form>
 </div>
 
-<?php require_once(realpath(dirname(__FILE__) . "/private/footer.php")); ?>
+<?php require_once(realpath(dirname(__DIR__) . "/private/footer.php")); ?>
