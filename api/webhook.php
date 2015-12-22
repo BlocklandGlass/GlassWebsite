bd865e5a6b7e31abb45c22437ccf6300e9dd66ae
Jincux <dephurites@gmail.com>
2015-12-22 13:48:19 -0500

    updated response

--- api/webhook.php
+++ api/webhook.php
@@ -1,8 +1,11 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
-  shell_exec( $cmd = 'cd ' . realpath(dirname(__DIR__)) . ' && git reset --hard HEAD && git pull' );
-  $str = $cmd . "\n" . $_POST['payload'];
+  $res = shell_exec( $cmd = 'cd ' . realpath(dirname(__DIR__)) . ' && git reset --hard HEAD && git pull' );
+  $str = $cmd . "\n\n" . $res . "\n\n" . $_POST['payload'];
   file_put_contents('./payload.info', $str);
+
+  header('Content-Type: text/text');
+  echo $cmd . "\n\n" . $res;
 }
 ?>
