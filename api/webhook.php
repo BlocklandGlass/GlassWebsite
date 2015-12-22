c4133028d4ccd93d18dd5919285c7646ef7bf78a
Jincux <dephurites@gmail.com>
2015-12-22 13:41:21 -0500

    payload logging

--- api/webhook.php
+++ api/webhook.php
@@ -1,6 +1,8 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd ' . realpath(dirname(__DIR__)) . ' && git reset --hard HEAD && git pull' );
+  shell_exec( $cmd = 'cd ' . realpath(dirname(__DIR__)) . ' && git reset --hard HEAD && git pull' );
+  $str = $cmd . "\n" . $_POST['payload'];
+  file_put_contents('./payload.info', $str);
 }
 ?>
