9d74e9bb5ce48c34f3ad0b2ef398523693134c8e
Devon Endicott <devon.endicott@gmail.com>
2016-05-31 14:21:31 -0400

    extra backslash

--- ajax/getTrendingAddons.php
+++ ajax/getTrendingAddons.php
@@ -26,7 +26,7 @@
 		<tr style="background-color:#<?php echo $col;?>; border-radius: 15px; padding: 5px; margin:5px; display:block;<?php echo $med ? "box-shadow: 1px 2px 3px #888888;" : "" ?> ">
 			<td style="padding: 10px; width: 20px;font-family: Impact; font-size:1.5em"><?php echo $index+1; ?></td>
 			<td style="line-height: 1em;"><a href="/addons/addon.php?id=<?php echo $addon->id?>"><?php echo htmlspecialchars($addon->name) ?></a> by
-				<a href=\"/user/view.php?blid=<?php echo $user->blid ?>"><?php echo htmlspecialchars($user->username) ?></a><br />
+				<a href="/user/view.php?blid=<?php echo $user->blid ?>"><?php echo htmlspecialchars($user->username) ?></a><br />
 				<span style="font-weight: bold; font-size: .6em"><?php echo $addon->getTotalDownloads() ?> downloads</span></td>
 		</tr>
 		<?php
