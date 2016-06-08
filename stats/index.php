4ae1a64098c12f56d7d1436618a6716c6083081a
Devon Endicott <devon.endicott@gmail.com>
2016-06-07 23:55:48 -0400

    include lib

---
+++ stats/index.php
@@ -0,0 +1,96 @@
+<?php
+	require_once dirname(__DIR__) . "/private/class/GroupManager.php";
+	require_once dirname(__DIR__) . "/private/class/UserManager.php";
+	require_once dirname(__DIR__) . "/private/class/CronStatManager.php";
+
+	$_PAGETITLE = "Glass | Add-Ons";
+
+	include(realpath(dirname(__DIR__) . "/private/header.php"));
+	include(realpath(dirname(__DIR__) . "/private/navigationbar.php"));
+
+	$user = UserManager::getCurrent();
+
+  $csm = new CronStatManager();
+  $data = $csm->getRecentBlocklandStats();
+?>
+<div class="maincontainer">
+  <canvas id="myChart" style="width:400px;height:400px"></canvas>
+  <script>
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: <?php
+					$res = array();
+					foreach($data as $time=>$dat) {
+						date_default_timezone_set('EST');
+						$res[] = date("H:i:s e", strtotime($dat->servers . " UTC"));
+					}
+					echo json_encode($res);
+					?>,
+          datasets: [{
+            label: "Players",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'round',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: <?php
+            $res = array();
+            foreach($data as $time=>$dat) {
+              $res[] = $dat->users;
+            }
+            echo json_encode($res);
+            ?>,
+          },{
+            label: "Servers",
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "rgba(255,0,0,0.4)",
+            borderColor: "rgba(255,0,0,0.4)",
+            borderCapStyle: 'round',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(255,0,0,0.4)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(255,0,0,0.4)",
+            pointHoverBorderColor: "rgba(255,0,0,0.4)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: <?php
+            $res = array();
+            foreach($data as $time=>$dat) {
+              $res[] = $dat->servers;
+            }
+            echo json_encode($res);
+            ?>,
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+  </script>
+</div>
