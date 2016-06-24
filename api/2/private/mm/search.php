4db163153f8ab96eee4f0a702282360c0246d572
Devon Endicott <devon.endicott@gmail.com>
2016-06-24 16:03:31 -0400

    wrong func

--- api/2/private/mm/search.php
+++ api/2/private/mm/search.php
@@ -25,7 +25,7 @@ if($type == "addon") {
     $r->type = "addon";
     $r->title = $addon->getName();
     //$r->author
-    $r->description = addcslashes($addon->getDescription());
+    $r->description = addslashes($addon->getDescription());
     $ret->results[] = $r;
   }
 } else {
