6d277d369056752f9148b068806793453108f690
Devon Endicott <devon.endicott@gmail.com>
2016-06-28 00:49:21 -0400

    download count import script

---
+++ private/script/importDownloadCounts.php
@@ -0,0 +1,22 @@
+<?php
+require_once dirname(__DIR__) . '/class/DatabaseManager.php';
+
+$db = new DatabaseManager();
+
+$json = file_get_contents("http://blocklandglass.com/downloadsExport.php");
+$data = json_decode($json, true);
+
+foreach($data as $id=>$dat) {
+  $web = $dat['downloads_web'];
+  $ig = $dat['downloads_ingame'];
+  $up = $dat['downloads_update'];
+  $total = $web+$ig+$up;
+
+  $db->query("UPDATE `addon_stats` SET `totalDownloads` = (`totalDownloads` + $total),
+  `webDownloads` = (`webDownloads` + $web),
+  `ingameDownloads` = (`ingameDownloads` + $ig),
+  `updateDownloads` = (`updateDownloads` + $up) WHERE `aid`=$id");
+  echo($db->error());
+}
+
+?>
