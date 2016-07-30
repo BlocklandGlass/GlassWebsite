6fd70a0c6a534da387ce0304be3bb89bfb840b3d
Devon Endicott <devon.endicott@gmail.com>
2016-07-30 10:42:20 -0400

    semicolon

--- private/class/AddonUpdateObject.php
+++ private/class/AddonUpdateObject.php
@@ -59,7 +59,7 @@ class AddonUpdateObject {
 			if(!is_file($fildOld)) {
 				$path = realpath(dirname(__DIR__) . '/../addons/files/local/');
 				$fh = fopen($path . $this->aid . '.zip', 'w');
-				$ch = curl_init()
+				$ch = curl_init();
 				curl_setopt($ch, CURLOPT_URL, "http://cdn.blocklandglass.com/addons/" . $this->aid . "_1");
 				curl_setopt($ch, CURLOPT_FILE, $fh);
 				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // this will follow redirects
