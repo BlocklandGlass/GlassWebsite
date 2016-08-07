3e7bfa6c22ab9e800f1e5e6bcd4a3a58a68294b3
Devon Endicott <devon.endicott@gmail.com>
2016-08-07 12:56:49 -0400

    user list formatting

--- stats/users.php
+++ stats/users.php
@@ -11,9 +11,50 @@
 
 	$users = UserLog::getRecentlyActive();
 ?>
+<style>
+.list td {
+  padding: 10px;
+}
+
+.list tr:nth-child(2n+1) td {
+  background-color: #ddd;
+}
+
+.list tr:first-child td {
+  background-color: #777;
+  color: #fff;
+  font-weight: bold;
+}
+
+.list tr td:first-child {
+  border-radius: 10px 0 0 10px;
+}
+
+.list tr td:last-child {
+  border-radius: 0 10px 10px 0;
+}
+
+.list {
+  margin: 0 auto;
+}
+
+.maincontainer p {
+  text-align: center;
+}
+
+form {
+  text-align: center;
+}
+
+</style>
 <div class="maincontainer">
-	<table>
-		<tbody>
+	<table class="list">
+    <tbody>
+      <tr>
+        <td>Username</td>
+        <td>BL_ID</td>
+        <td>Version</td>
+      </tr>
 			<?php foreach($users as $u) {
 		    echo "<tr><td><b>" . UserLog::getCurrentUsername($u->blid) . "</b></td><td>" . $u->blid . "</td><td>" . StatUsageManager::getVersionUsed($u->blid, 11) . "</td></tr>";
 		  } ?>
