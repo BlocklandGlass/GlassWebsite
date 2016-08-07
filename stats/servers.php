517a102104ba773e8e9c58c201e0c910c982fe9e
Devon Endicott <devon.endicott@gmail.com>
2016-08-07 13:00:58 -0400

    server list formatting

--- stats/servers.php
+++ stats/servers.php
@@ -10,12 +10,48 @@
 
 	$servers = ServerTracker::getActiveServers();
 ?>
+<style>
+.list td {
+  padding: 10px;
+}
+
+.list tr:nth-child(2n+1) td {
+  background-color: #ddd;
+}
+
+.list tr:first-child td {
+  background-color: #777;
+  color: #fff;
+  font-weight: bold;
+}
+
+.list tr td:first-child {
+  border-radius: 10px 0 0 10px;
+}
+
+.list tr td:last-child {
+  border-radius: 0 10px 10px 0;
+}
+
+.list {
+  margin: 0 auto;
+}
+
+.maincontainer p {
+  text-align: center;
+}
+
+form {
+  text-align: center;
+}
+
+</style>
 <div class="maincontainer">
-	<table>
+	<table class="list">
 		<tbody>
 			<tr>
-				<th>Host/Server</th>
-				<th>Users</th>
+				<td>Host/Server</td>
+				<td>Users</td>
 			</tr>
 			<?php foreach($servers as $s) {
 		    echo "<tr><td style=\"vertical-align: top\"><b>" . $s->host . "</b> " . $s->ip . ":" . $s->port . "</td>";
