26f111af351fa08a9da31c0b08f496ec8ab9b50a
Devon Endicott <devon.endicott@gmail.com>
2016-10-19 16:54:40 -0400

    corrected imports

--- api/2/private/mm/home.php
+++ api/2/private/mm/home.php
@@ -1,5 +1,7 @@
 <?php
 require_once dirname(__DIR__) . "/../../../private/class/AddonManager.php";
+require_once dirname(__DIR__) . "/../../../private/class/UserLog.php";
+require_once dirname(__DIR__) . "/../../../private/class/UserManager.php";
 
 /*
 
