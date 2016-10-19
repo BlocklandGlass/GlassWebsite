3ef7bb7c9126cda1c7956f653176ff4082e277c7
Devon Endicott <devon.endicott@gmail.com>
2016-10-19 15:49:39 -0400

    tweaked auth

--- api/2/private/BlocklandAuth.php
+++ api/2/private/BlocklandAuth.php
@@ -1,51 +1,50 @@
 <?php
 class BlocklandAuth {
   public static function checkAuth($username, $ip, $blid) {
-    $authData = BlocklandAuth::auth($username, $ip);
-    if($authData[0] == "success") {
-      if($authData[1] == $blid) {
-        return true;
-      } else {
-        return false;
-      }
+    $res = BlocklandAuth::auth($username, $ip);
+    if($res !== false) {
+      return $res == $blid;
     } else {
-      return $authData;
+      return false;
     }
   }
 
   public static function auth($name, $ip) {
-    $url = 'http://auth.blockland.us/authQuery.php';
-		$data = array('NAME' => $name, 'IP' => $ip);
-		$options = array(
-		        'http' => array(
-		        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-		        'method'  => 'POST',
-		        'content' => http_build_query($data),
-		    )
-		);
-
     try {
-  		$context  = stream_context_create($options);
-  		$result = @file_get_contents($url, false, $context);
-
-      if($result === false) {
-        //unable to open connection_status
-        return array("connection_failed");
-      }
-
-  		if(strpos($result, "NO") === 0) {
-  			return array("rejected");
-  		} else if(strpos($result, "YES") === 0) {
-  			$words = explode(" ", $result);
-  			return array("success", $word[1]);
-  		} else if(strpos($result, "ERROR") === 0) {
-  			return array("blauth_error");
-  		} else {
-  			return array("unhandled_error");
-  		}
+  		$result = BlocklandAuthenticate($name);
+  		return $result;
     } catch(Exception $e) {
+      error_log("BlocklandAuth auth error: " . $e);
       return false;
     }
   }
+}
+
+function BlocklandAuthenticate($username) {
+	$username = mb_convert_encoding(urldecode($username), "ISO-8859-1");
+	$username = str_replace("%", "%25", $username);
+	$encodeChars = array(" ", "@", "$", "&", "?", "=", "+", ":", ",", "/");
+	$encodeValues = array("%20", "%40", "%24", "%26", "%3F", "%3D", "%2B", "3A","%2C", "%2F");
+	$username = str_replace($encodeChars, $encodeValues, $username);
+
+	$postData = "NAME=${username}&IP=${_SERVER['REMOTE_ADDR']}";
+
+	$opts = array('http' => array('method' => 'POST', 'header' => "Connection: keep-alive\r\nUser-Agent: Blockland-r1986\r\nContent-type: application/x-www-form-urlencoded\r\nContent-Length: ". strlen($postData) . "\r\n", 'content' => $postData));
+
+	$context  = stream_context_create($opts);
+	$result = file_get_contents('http://auth.blockland.us/authQuery.php', false, $context);
+	$parsedResult = explode(' ', trim($result));
+
+	if($parsedResult[0] == "NO")
+		return false;
+
+	else if(!is_numeric($parsedResult[1]))
+	{
+		print($result);
+		return false;
+	}
+
+	else
+		return intval($parsedResult[1]);
 }
 ?>
