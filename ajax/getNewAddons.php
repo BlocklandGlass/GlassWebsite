4140d7d3b5e721df0eb4187d05e110b20d110453
Joshua Olive <joshmolive@gmail.com>
2016-11-04 19:21:56 +0000

    trending/new addons fix attempt 2

--- ajax/getNewAddons.php
+++ ajax/getNewAddons.php
@@ -14,8 +14,8 @@
 		?>
 		<tr style="background-color:#<?php echo $col;?>; border-radius: 15px; padding: 5px; margin:5px; display:block;">
 			<td style="padding: 10px; width: 20px;font-family: Impact, HelveticaNeue-CondensedBold, Helvetica Neue; font-size:1.5em"><?php echo $index+1; ?></td>
-			<td style="line-height: 1em;"><a href="/addons/addon.php?id=<?php echo $addon->id?>"><?php echo htmlspecialchars($addon->name) ?></a> in
-				<a href="/addons/board.php?id=<?php echo $addon->board?>"><?php echo($addon->board); ?></a><br />
+			<td style="line-height: 1em;"><a href="/addons/addon.php?id=<?php echo $addon->id?>"><?php echo htmlspecialchars($addon->getName()) ?></a> in
+				<a href="/addons/board.php?id=<?php echo $addon->getBoard() ?>"><?php echo BoardManager::getFromID($addon->getBoard())->getName() ?></a><br />
 				<span style="font-weight: bold; font-size: .6em"><?php echo date("M jS Y, g:i a", strtotime($addon->uploadDate)) ?></span></td>
 		</tr>
 		<?php
