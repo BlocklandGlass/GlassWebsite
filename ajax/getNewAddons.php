3b7c358c5ec48bea4550dd90a24e28a8c83afc35
Joshua Olive <joshmolive@gmail.com>
2016-11-04 19:27:22 +0000

    nevermind

--- ajax/getNewAddons.php
+++ ajax/getNewAddons.php
@@ -15,7 +15,7 @@
 		<tr style="background-color:#<?php echo $col;?>; border-radius: 15px; padding: 5px; margin:5px; display:block;">
 			<td style="padding: 10px; width: 20px;font-family: Impact, HelveticaNeue-CondensedBold, Helvetica Neue; font-size:1.5em"><?php echo $index+1; ?></td>
 			<td style="line-height: 1em;"><a href="/addons/addon.php?id=<?php echo $addon->id?>"><?php echo htmlspecialchars($addon->getName()) ?></a> in
-				<a href="/addons/board.php?id=<?php echo $addon->getBoard() ?>"><?php echo BoardManager::getFromID($addon->getBoard())->getName() ?></a><br />
+				<a href="/addons/board.php?id=<?php echo $addon->getBoard() ?>"><?php echo Category ?></a><br />
 				<span style="font-weight: bold; font-size: .6em"><?php echo date("M jS Y, g:i a", strtotime($addon->uploadDate)) ?></span></td>
 		</tr>
 		<?php
