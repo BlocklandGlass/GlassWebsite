b4b021dfa2228ee16db19b12b4aec08e7a2a49e7
Devon Endicott <devon.endicott@gmail.com>
2016-12-09 18:11:54 -0500

    utf8 encoding

--- api/3/private/mm/rtbaddon.php
+++ api/3/private/mm/rtbaddon.php
@@ -5,4 +5,8 @@ $ret = new stdClass();
 $ret->status = "success";
 $ret->addon = RTBAddonManager::getAddonFromId($_REQUEST['id']);
 
+$ret->addon->author = utf8_encode($ret->addon->author);
+$ret->addon->description = utf8_encode($ret->addon->description);
+$ret->addon->title = utf8_encode($ret->addon->title);
+
 echo json_encode($ret, JSON_PRETTY_PRINT);
