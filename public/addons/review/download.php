b124008fb6a5131d83a464262832aadc66402626
Jincux <devon.endicott@gmail.com>
2017-01-16 14:45:59 -0500

    download

---
+++ public/addons/review/download.php
@@ -0,0 +1,40 @@
+<?php
+require dirname(__DIR__) . '/../../private/autoload.php';
+//requirements
+use Glass\AddonManager;
+
+$file = $_REQUEST['file'] ?? "";
+
+$fp = fopen(dirname(__DIR__) . "/../../filebin/" . $file, 'r');
+if(!$fp) {
+  $result->status = "error";
+  $result->error = "Failed to open resource";
+  die(json_encode($result, JSON_PRETTY_PRINT));
+}
+
+$contents = '';
+while(!feof($fp)) {
+  $contents .= fread($fp, 2);
+}
+fclose($fp);
+
+$ext = substr($file, strrpos($file, ".")+1);
+$contentTypes = [
+  "png" => "image/png",
+  "jpg" => "image/jpeg",
+  "jpeg" => "image/jpeg",
+];
+
+if(isset($contentTypes[$ext])) {
+  header('Content-Type: ' . $contentTypes[$ext]);
+  echo($contents);
+} else {
+  if(!ctype_print($contents)) {
+    header('Content-Type: application/ocelot-stream');
+    header('Content-Disposition: attachment; filename=' . substr($file, strrpos($file, "/")+1));
+  }
+  echo($contents);
+  //$result->status = "error";
+  //$result->error = "Unsupported filetype";
+  die(json_encode($result, JSON_PRETTY_PRINT));
+}
