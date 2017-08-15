8dfab179fee5cf0281901f7950d52d0011796af7
Jincux <devon.endicott@gmail.com>
2017-08-15 17:35:47 -0400

    tos

--- public/register.php
+++ public/register.php
@@ -15,23 +15,28 @@
 	<div class="center" id="registerStatus" style="display: none;">
 		<?php echo("<p>" . utf8_encode($registerStatus['message']) . "</p>"); ?>
 	</div>
-	<form action="/register.php" method="post" id="mainRegisterForm">
-		<table class="formtable">
-			<tbody>
-				<tr><td class="center" colspan="2"><h2>Register</h2></td></tr>
-				<tr><td>E-Mail Address:</td><td><input type="text" name="email" id="email"></td></tr>
-				<tr><td>BLID:</td><td><input type="text" name="blid" id="blid"></td></tr>
-				<tr><td>Password:</td><td><input type="password" name="password" id="password"></td></tr>
-				<tr><td>Verify Password:</td><td><input type="password" name="verify" id="verify"></td></tr>
-				<tr><td class="center" colspan="2"><input type="submit"></td></tr>
-			</tbody>
-		</table>
-		<?php
+	<div class="tile" style="max-width: 50%; margin: 0 auto;">
+		<form action="/register.php" method="post" id="mainRegisterForm">
+			<table class="formtable">
+				<tbody>
+					<tr><td class="center" colspan="2"><h2>Register</h2></td></tr>
+					<tr><td>E-Mail Address:</td><td><input type="text" name="email" id="email"></td></tr>
+					<tr><td>BLID:</td><td><input type="text" name="blid" id="blid"></td></tr>
+					<tr><td>Password:</td><td><input type="password" name="password" id="password"></td></tr>
+					<tr><td>Verify Password:</td><td><input type="password" name="verify" id="verify"></td></tr>
+					<tr><td class="center" colspan="2"><input type="submit"></td></tr>
+				</tbody>
+			</table>
+			<p>
+			By registering an account, you are agreeing to the <a href="/tos.php">Terms of Service</a>
+			</p>
+			<?php
 			if(isset($_POST['redirect'])) {
-				echo("<input type=\"hidden\" name=\"redirect\" value=\"" . htmlspecialchars($_POST['redirect']) . "\">");
+			echo("<input type=\"hidden\" name=\"redirect\" value=\"" . htmlspecialchars($_POST['redirect']) . "\">");
 			}
-		?>
-	</form>
+			?>
+		</form>
+	</div>
 </div>
 <form class="hidden" action="/login.php" method="post" id="redirectToLoginForm">
 <?php
