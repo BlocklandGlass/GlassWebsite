95e6cc7d7397a747401d1ca111403b77f93c40a1
Jincux <devon.endicott@gmail.com>
2018-01-01 22:31:23 -0500

    dl.php changes

--- public/dl.php
+++ public/dl.php
@@ -5,20 +5,29 @@
 	use Glass\AddonManager;
 ?>
 <div class="maincontainer">
-	<p>
+	<div class="tile">
 		<h2>Download</h2>
-		In addition to offering add-on services online, Glass offers a variety of in-game features. You'll be able to automatically install your downloaded add-ons (as well as keep them up to date), be able to talk to other people playing Blockland and manage your servers' preferences.
-	</p>
-  <?php
-  $glassAddonId = 11; //this needs to be changed before going live, or we need a "find addon by name"
-  $id = "stable";
-  $class = "green";
-  $ao = AddonManager::getFromId($glassAddonId);
-  $version = $ao->getVersion();
-  ?>
+		<p>
+			In addition to offering add-on services online, Glass offers a variety of in-game features. You'll be able to automatically install your downloaded add-ons (as well as keep them up to date), be able to talk to other people playing Blockland and manage your servers' preferences.
+		</p>
+		<h3>Steam</h3>
+		<p>
+			Save <i>System_BlocklandGlass.zip</i> in "C:/Program Files (x86)/Steam/steamapps/common/Blockland/Add-Ons"
+		</p>
+		<h3>Non-Steam</h3>
+		<p>
+			Save <i>System_BlocklandGlass.zip</i> in "My Documents/Blockland/Add-Ons"
+		</p>
+	  <?php
+	  $glassAddonId = 11; //this needs to be changed before going live, or we need a "find addon by name"
+	  $class = "green";
+	  $ao = AddonManager::getFromId($glassAddonId);
+	  $version = $ao->getVersion();
+	  ?>
+	</div>
   <div style="text-align: center">
     <?php
-    echo '<a href="/addons/download.php?id=' . $glassAddonId . '&beta=0" class="btn dlbtn ' . $class . '"><b>' . ucfirst($id) . '</b><span style="font-size:9pt"><br />v' . $version . '</span></a>';
+    echo '<a href="/addons/download.php?id=' . $glassAddonId . '&beta=0" class="btn dlbtn ' . $class . '"><b>Download</b><span style="font-size:9pt"><br />v' . $version . '</span></a>';
     ?>
   </div>
 </div>
