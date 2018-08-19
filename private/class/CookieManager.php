31d52a42ab448a42c1ca677ad48785627b8e5dff
Joshua Olive <12955300+paperworx@users.noreply.github.com>
2018-08-19 16:37:30 +0100

    Revert "Embed UserAgentParser"
    This reverts commit dcd9e056382540fdeab15806210adf116638981e.

--- private/class/CookieManager.php
+++ private/class/CookieManager.php
@@ -3,7 +3,6 @@
 namespace Glass;
 
 use \Glass\UserManager;
-require_once(realpath(dirname(__DIR__) . "/../private/lib/UserAgentParser.php"));
 
 /** Uses cookies (instead of non-expiring PHP sessions) to
  * provide session persistence */
