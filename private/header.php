710c7471cd3985ff8c31b844c07b872b25f6ae2a
Joshua Olive <12955300+paperworx@users.noreply.github.com>
2018-08-19 16:35:25 +0100

    Whoops

--- private/header.php
+++ private/header.php
@@ -92,17 +92,17 @@
     <meta name="og:image" content="/img/logo2.png" />
 		<?php
 			if(isset($_PAGETITLE)) {
-				echo '<title>' . $_PAGETITLE . '</title>';
+				echo '<title>' . htmlspecialchars($_PAGETITLE) . '</title>';
 			} else {
 				echo '<title>Blockland Glass</title>';
 			}
 
       if(isset($_PAGETITLE)) {
-        echo '<meta name="og:title" content="' . str_replace("Blockland Glass | ", "", $_PAGETITLE) . '" />';
+        echo '<meta name="og:title" content="' . htmlspecialchars(str_replace("Blockland Glass | ", "", $_PAGETITLE)) . '" />';
       }
 
       if(isset($_PAGEDESCRIPTION)) {
-        echo '<meta name="og:description" content="' . mb_strimwidth($_PAGEDESCRIPTION, 0, 140, "...") . '" />';
+        echo '<meta name="og:description" content="' .  htmlspecialchars(mb_strimwidth($_PAGEDESCRIPTION, 0, 140, "...")) . '" />';
       } else {
         echo '<meta name="og:description" content="Social and content platform for Blockland." />';
       }
