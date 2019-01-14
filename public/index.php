1a4cd782fed8b0a7afdf9fc0c3be3cf53e064346
Devon Endicott <devon.endicott@gmail.com>
2019-01-13 20:34:48 -0500

    force equal sizing on home page

--- public/index.php
+++ public/index.php
@@ -68,7 +68,7 @@ setInterval(nextImage, 5000);
 		<div style="padding-top: 50px;">
 			<image style="margin: 10px 0px 5px 0px; max-width: 100%" src="https://blocklandglass.com/img/logoWhite.png" />
 			<h2 style="font-weight: normal">A service for the community, by the community</h2>
-			<a href="dl.php" class="btn blue"><strong>Download</strong></a>
+			<a href="dl.php" class="btn blue" style="width: 150px">Download</a>
 			<a href="addons" class="btn yellow" style="width: 150px">Add-Ons</a><br /><br />
 		</div>
 	</div>
